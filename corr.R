b7f1552ed174ae985d346635dfedca363e816fc2
marishalittle <marishalittle@gmail.com>
2015-12-20 23:02:26 +0500

    Update corr.R

--- corr.R
+++ corr.R
@@ -1,16 +1,17 @@
 corr <- function(directory, threshold = 0) {
-        files_full <- list.files(directory, full.names = TRUE)
-        dat <- vector(mode = "numeric", length = 0)
-        
-        for (i in 1:length(files_full)) {
-                moni_i <- read.csv(files_full[i])
-                csum <- sum((!is.na(moni_i$sulfate)) & (!is.na(moni_i$nitrate)))
-                if (csum > threshold) {
-                        tmp <- moni_i[which(!is.na(moni_i$sulfate)), ]
-                        submoni_i <- tmp[which(!is.na(tmp$nitrate)), ]
-                        dat <- c(dat, cor(submoni_i$sulfate, submoni_i$nitrate))
-                }
-        }
-        
-        dat
+  files <- list.files(directory, full.names = TRUE);
+  correlationList <- c();
+  index <- 1;
+  
+  while (index <= length(files)) {
+    completeCases <- complete(directory, index);
+    
+    if (completeCases$nobs > threshold) {
+      data <- read.csv(files[index], comment.char = "");
+      correlationList <- c(correlationList, cor(data$sulfate, data$nitrate, use = "complete.obs"));
+    }
+    index <- index + 1;
+  }
+  
+  correlationList;
 }
